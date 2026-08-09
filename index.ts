2cb5e3a7af2400a0aa1d1d466f250db64d6e6871
Brian E <briatt22@gmail.com>
2026-08-09 16:46:32 -0500

    Introduce basic slash commands

--- index.ts
+++ index.ts
@@ -1,14 +1,20 @@
-import { Client, GatewayIntentBits, Events } from 'discord.js';
+import { Client, GatewayIntentBits, Events, Collection } from 'discord.js';
 import dotenv from 'dotenv';
+import initializeCommands from './commandHandler.js';
+
 
 dotenv.config();
+
+type ClientWithCommands = Client & {
+  commands?: Collection<string, any>;
+};
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages
   ],
-});
+}) as ClientWithCommands;
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
@@ -21,5 +27,10 @@ client.on(Events.MessageCreate, async (message) => {
     await message.reply('Pong!');
   }
 });
+
+export default client as ClientWithCommands;
+
+await initializeCommands();
+
 
 client.login(process.env.DISCORD_TOKEN);
